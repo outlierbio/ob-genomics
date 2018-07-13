5575d59ed693bb65d1e78a815fdee027eb47711a
Jake Feala <jake@outlierbio.com>
2018-07-12 20:05:20 -0400

    Refactor app --> push all data access to database.r

--- app.r
+++ app.r
@@ -8,97 +8,66 @@ source('plots.r')
 genes <- c('GAPDH', 'TP53', 'KRAS', 'MYC')
 
 ui <- fluidPage(
-	titlePanel(title = 'Celsius public genomics app', windowTitle = 'Genomics app'),
+	titlePanel(title = 'Celsius genomics app', windowTitle = 'Celsius genomics'),
 	tabsetPanel(type='tabs',
 
-	  tabPanel('Gene',
-       sidebarPanel(
-         selectizeInput(inputId = 'gene', label = 'Select gene(s)',
-                        choices = genes, selected = c('GAPDH', 'MYC', 'TP53'),
-                        multiple = TRUE)
-       ),
-       mainPanel(
-         h2('TCGA expression by cohort'),
-         plotOutput('tcga_expr', height = 400),
-         h2('TCGA mutations by cohort'),
-         plotOutput('tcga_mut', height = 400),
-         h2('TCGA expression by immune subtype'),
-         plotOutput('expr_immune_subtype', height=400),
-         h2('GTEx expression by tissue'),
-         plotOutput('gtex_expr', height=400),
-         h2('HPA expression by cell type'),
-         plotOutput('hpa_prot', height=400),
-         h2('GTEx vs HPA mRNA expression'),
-         plotlyOutput('gtex_vs_hpa', height=400),
-         h2('HPA protein vs mRNA expression'),
-         plotlyOutput('hpa_prot_vs_expr', height=400)
-       )
+    tabPanel('Tissue',
+      sidebarPanel(
+        selectizeInput(inputId = 'gene', label = 'Select gene(s)',
+                       choices = genes, selected = c('GAPDH', 'MYC', 'TP53'),
+                       multiple = TRUE)
+      ),
+      mainPanel(
+        h2('GTEx expression by tissue'),
+        plotOutput('gtex_expr', height=400),
+        h2('HPA expression by cell type'),
+        plotOutput('hpa_prot', height=400),
+        h2('GTEx vs HPA mRNA expression'),
+        plotlyOutput('gtex_vs_hpa', height=400),
+        h2('HPA protein vs mRNA expression'),
+        plotlyOutput('hpa_prot_vs_expr', height=400)
+      )
+    ),
+
+    tabPanel('Cancer',
+      sidebarPanel(
+        selectizeInput(inputId = 'gene', label = 'Select gene(s)',
+                       choices = genes, selected = c('GAPDH', 'MYC', 'TP53'),
+                       multiple = TRUE)
+      ),
+      mainPanel(
+        h2('TCGA expression by cohort'),
+        plotOutput('tcga_expr', height = 400),
+        h2('TCGA mutations by cohort'),
+        plotOutput('tcga_mut', height = 400)
+      )
+    ),
+    
+    tabPanel('Immuno-oncology',
+      sidebarPanel(
+        selectizeInput(inputId = 'gene', label = 'Select gene(s)',
+                       choices = genes, selected = c('GAPDH', 'MYC', 'TP53'),
+                       multiple = TRUE)
+      ),
+      mainPanel(
+        h2('TCGA expression by immune subtype'),
+        plotOutput('expr_immune_subtype', height=400)
+      )
     )
-# 
-# 	  tabPanel('Correlations',
-#        sidebarPanel(
-#          selectizeInput(inputId = 'in2', label = 'Select a gene',
-#                         choices = genes, selected = c('GAPDH'), multiple = FALSE),
-#          selectizeInput(inputId = 'in3', label = 'Select another variable',
-#                         choices = c('val1', 'val2'), selected = 'val1', multiple = FALSE)
-#          uiOutput('generated_list_input'),
-#          sliderInput("threshold", "A threshold",  
-#                      min = min(df$var3), max = max(df$var3, value = 1)
-#        ),
-#        mainPanel(
-#          tabsetPanel(type='pills',
-#             tabPanel('A table',         
-#               dataTableOutput('table1')
-#             ),
-#             tabPanel('Interactive figure',
-#                plotlyOutput('fig2', height = 700),
-#                h1(' ')
-#             )
-#          )
-#        )
-#     )
+    
   )
 )
 
 server <- function(input, output, session) {
 
-  tcga_expr <- reactive({
-    sample_expression %>%
-      filter(symbol %in% input$gene) %>%
-      collect %>%
-      spread(unit, value)
-  })
-
-  gtex_expr <- reactive({
-    tissue_expression %>%
-      filter(symbol %in% input$gene) %>%
-      collect %>%
-      spread(unit, value)
-  })
-  
-  hpa_prot <- reactive({
-    cell_type_protein %>%
-      filter(symbol %in% input$gene) %>%
-      collect %>%
-      spread(unit, value) %>%
-      mutate(
-        `detection level` = factor(`detection level`,
-                                   levels=c('Not detected', 'Low', 'Medium', 'High')))
-  })
-  
-  tcga_mut <- reactive({
-    sample_mutation %>%
-      filter(symbol %in% input$gene) %>%
-      collect %>%
-      spread(data_type, value)
-  })
+  tcga_expr <- reactive(tcga_expr_by_gene(input$gene))
+  gtex_expr <- reactive(gtex_expr_by_gene(input$gene))
+  hpa_expr <- reactive(hpa_expr_by_gene(input$gene))
+  hpa_prot <- reactive(hpa_prot_by_gene(input$gene))
+  tcga_mut <- reactive(tcga_mut_by_gene(input$gene))
   
 	output$tcga_expr <- renderPlot({
-	  p <- tcga_expr() %>%
-      plot_tcga_by_cohort  # ggplot (flat)
-	  p
-	  # ggplotly(p, tooltip='text') %>%  # Convert to plotly with labeled tooltip
-	  #   layout(margin = list(b=100))  # Adjust plot margins
+	  tcga_expr() %>% plot_tcga_by_cohort
 	})
 	
 	output$gtex_expr <- renderPlot({
@@ -106,22 +75,15 @@ server <- function(input, output, session) {
 	})
 
 	output$gtex_vs_hpa <- renderPlotly({
-	  hpa <- hpa_expr() %>% 
-	    select(tissue, subtype, symbol, HPA = TPM)
-	  p <- gtex_expr() %>%
-	    select(tissue, subtype, symbol, GTEx = median_tpm) %>%
-	    inner_join(hpa) %>%
-	    plot_gtex_vs_hpa
-	  ggplotly(p)
+	    gtex_vs_hpa_by_gene(input$gene) %>%
+	      plot_gtex_vs_hpa %>%
+	      ggplotly
 	})
 	
 	output$hpa_prot_vs_expr <- renderPlotly({
-	  prot <- hpa_prot() %>%
-	    select(tissue, subtype, symbol, cell_type_id, `detection level`)
-	  p <- hpa_expr() %>%
-	    select(tissue, subtype, symbol, TPM) %>%
-	    inner_join(prot) %>%
-      plot_hpa_prot_vs_expr
+	  hpa_prot_vs_expr_by_gene(input$gene) %>%
+      plot_hpa_prot_vs_expr %>%
+	    ggplotly
 	})
 	
 	output$hpa_prot <- renderPlot({
@@ -129,8 +91,7 @@ server <- function(input, output, session) {
 	})
 	
 	output$expr_immune_subtype <- renderPlot({
-	  tcga_expr() %>%
-	    inner_join(patient_immune_subtype %>% collect) %>%
+	  expr_immune_subtype_by_gene(input$gene) %>%
 	    plot_expr_by_immune_subtype
 	})
 	
--- database.r
+++ database.r
@@ -3,10 +3,12 @@ library(RSQLite)
 library(tidyverse)
 library(tidyr)
 
-# Connect to DB and get tables
-
-# Entity tables
+# Connect to DB
 db <- DBI::dbConnect(RSQLite::SQLite(), 'ob_genomics.db')
+
+###############
+# Entity tables
+
 gene <- tbl(db, 'gene')
 cohort <- tbl(db, 'cohort')
 patient <- tbl(db, 'patient') %>%
@@ -17,54 +19,133 @@ cell_type <- tbl(db, 'cell_type') %>%
 sample <- tbl(db, 'sample') %>%
   inner_join(patient, by='patient_id')
 
+
+#############
 # Data tables
-patient_immune_subtype <- tbl(db, 'patient_text_value') %>%
-  filter(data_type == 'immune subtype') %>%
+
+data_tbls <- list(
+  patient_text_value = tbl(db, 'patient_text_value'),
+  patient_value = tbl(db, 'patient_value'),
+  sample_gene_value = tbl(db, 'sample_gene_value'),
+  sample_gene_text_value = tbl(db, 'sample_gene_text_value'),
+  tissue_gene_value = tbl(db, 'tissue_gene_value'),
+  cell_type_gene_text_value = tbl(db, 'cell_type_gene_text_value')
+)
+
+
+################
+# Derived tables
+
+patient_immune_subtype <- data_tbls$patient_text_value %>%
+  filter(data_type == 'Immune Subtype') %>%
   inner_join(patient, by='patient_id') %>%
   select(cohort_id, patient_id, immune_subtype=value)
 
-patient_clinical_text <- tbl(db, 'patient_text_value') %>%
+patient_clinical_text <- data_tbls$patient_text_value %>%
   filter(unit == 'clinical') %>%
   inner_join(patient, by='patient_id')
 
-patient_clinical_value <- tbl(db, 'patient_value') %>%
+patient_clinical_value <- data_tbls$patient_value %>%
   filter(unit == 'clinical') %>%
   inner_join(patient, by='patient_id')
 
-sample_expression <- tbl(db, 'sample_gene_value') %>%
+sample_expression <- data_tbls$sample_gene_value %>%
   filter(data_type == 'expression') %>%
   inner_join(gene, by='gene_id') %>%
   inner_join(sample, by='sample_id') 
 
-sample_copy_number <- tbl(db, 'sample_gene_value') %>%
+sample_copy_number <- data_tbls$sample_gene_value %>%
   filter(data_type == 'copy number') %>%
   inner_join(gene, by='gene_id') %>%
   inner_join(sample, by='sample_id') 
 
-sample_mutation <- tbl(db, 'sample_gene_text_value') %>%
+sample_mutation <- data_tbls$sample_gene_text_value %>%
   filter(unit == 'mutation') %>%
   inner_join(gene, by='gene_id') %>%
   inner_join(sample, by='sample_id')
 
-tissue_expression <- tbl(db, 'tissue_gene_value') %>%
+tissue_expression <- data_tbls$tissue_gene_value %>%
   filter(data_type == 'expression') %>%
   inner_join(gene, by='gene_id') %>%
   inner_join(tissue, by='tissue_id') 
 
-cell_type_protein <- tbl(db, 'cell_type_gene_text_value') %>%
+cell_type_protein <- data_tbls$cell_type_gene_text_value %>%
   filter(data_type == 'protein') %>%
   inner_join(gene, by='gene_id') %>%
   inner_join(cell_type, by='cell_type_id')
 
-tcga_clinical <- {
-  txt_pivoted <- patient_clinical_text %>%
+
+#######################
+# Data access functions
+
+get_tcga_clinical <- function() {
+  txt_pivoted <- data_tbls$patient_clinical_text %>%
     collect %>%
     spread(data_type, value)
   
-  num_pivoted <- patient_clinical_value %>%
+  num_pivoted <- data_tbls$patient_clinical_value %>%
     collect %>%
     spread(data_type, value)
   
   txt_pivoted %>%
     inner_join(num_pivoted)
+}
+
+tcga_expr_by_gene <- function(gene) {
+  sample_expression %>%
+    filter(symbol %in% gene) %>%
+    collect %>%
+    spread(unit, value)
+}
+
+gtex_expr_by_gene <- function(gene) {
+  tissue_expression %>%
+    filter(symbol %in% gene, source_id == 'GTEx') %>%
+    collect %>%
+    spread(unit, value)
+}
+
+hpa_expr_by_gene <- function(gene) {
+  tissue_expression %>%
+    filter(symbol %in% gene, source_id == 'HPA') %>%
+    collect %>%
+    spread(unit, value)
+}
+
+hpa_prot_by_gene <- function(gene) {
+  cell_type_protein %>%
+    filter(symbol %in% gene) %>%
+    collect %>%
+    spread(unit, value) %>%
+    mutate(
+      `detection level` = factor(`detection level`,
+                                 levels=c('Not detected', 'Low', 'Medium', 'High')))
+}
+
+gtex_vs_hpa_by_gene <- function(gene) {
+  hpa <- hpa_expr_by_gene(gene) %>% 
+    select(tissue, subtype, symbol, HPA = TPM)
+  gtex_expr_by_gene(gene) %>%
+    select(tissue, subtype, symbol, GTEx = median_tpm) %>%
+    inner_join(hpa)
+}
+
+hpa_prot_vs_expr_by_gene <- function(gene) {
+  prot <- hpa_prot_by_gene(gene) %>%
+    select(tissue, subtype, symbol, cell_type_id, `detection level`)
+  hpa_expr_by_gene(gene) %>%
+    select(tissue, subtype, symbol, TPM) %>%
+    inner_join(prot)
+}
+
+tcga_mut_by_gene <- function(gene) {
+  sample_mutation %>%
+    filter(symbol %in% gene) %>%
+    collect %>%
+    spread(data_type, value)
+}
+
+expr_immune_subtype_by_gene <- function(gene) {
+  tcga_expr_by_gene(gene) %>%
+    inner_join(patient_immune_subtype %>% collect)
 }
