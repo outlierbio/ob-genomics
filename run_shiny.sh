5129418fd1c4de084f7a7b681ff4674d8396c6ec
Jake Feala <jake@outlierbio.com>
2018-07-30 10:14:54 -0400

    Change to port 80

--- run_shiny.sh
+++ run_shiny.sh
@@ -4,7 +4,8 @@ docker build -f Dockerfile.shiny -t outlierbio/shiny .
 
 docker run \
 	--rm \
-	-p 88:3838 \
+	-d \
+	-p 80:3838 \
 	-v $DIR/apps:/srv/shiny-server \
     -v $DIR/logs:/var/log/shiny-server \
 	outlierbio/shiny
