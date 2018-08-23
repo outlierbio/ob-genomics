dcae40e850d025022865c055508184fb1879f589
Jake Feala <jake@outlierbio.com>
2018-08-23 05:47:18 -0400

    Add isoforms to app

--- apps/genomics/plots.r
+++ apps/genomics/plots.r
@@ -1,10 +1,24 @@
 plot_tcga_by_cohort <- function(df) {
-  print(df)
   ggplot(
     df,
     aes(x = cohort_id, y = log2(normalized_counts + 1), color = sample_type)
   ) +
     facet_grid(symbol ~ .) +
+    geom_boxplot(fill = NA) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0)) +
+    labs(
+      x = "",
+      y = "log2 normalized counts"
+    )
+}
+
+plot_tcga_isoform_by_cohort <- function(df) {
+  ggplot(
+    df,
+    aes(x = cohort_id, y = log2(as.numeric(normalized_counts) + 1), color = sample_type)
+  ) +
+    facet_grid(symbol + isoform_id ~ .) +
     geom_boxplot(fill = NA) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0)) +
@@ -63,6 +77,21 @@ plot_gtex_by_tissue <- function(df) {
     aes(x = tissue_id, y = log2(median_tpm + 1))
   ) +
     facet_grid(symbol ~ .) +
+    geom_point() +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1)) +
+    labs(
+      x = "",
+      y = "log2 median TPM"
+    )
+}
+
+plot_gtex_isoform_by_tissue <- function(df) {
+  ggplot(
+    df,
+    aes(x = tissue_id, y = log2(median_tpm + 1))
+  ) +
+    facet_grid(symbol + isoform_id ~ .) +
     geom_point() +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1)) +
